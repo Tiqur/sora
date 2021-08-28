dcac200138ff5ebdc5451a3a28d0e78695bab915
Tiqur <Tiqur7@gmail.com>
2021-08-28 02:19:42 -0700

    Fix unsigned right shift implementation

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ using namespace javarand;
 int main()
 {
   JavaRandom rand;
+  rand.setSeed(0);
+  std::cout << rand.nextInt() << std::endl;
   return 0;
 }
--- random.h
+++ random.h
@@ -8,12 +8,12 @@ namespace javarand
       const long mask = (1L << 48) -1;
 
       long initialScramble(long seed);
-      void setSeed(long seed);
       int next(int bits);
 
     public:
       long seed;
-
+      void setSeed(long seed);
+      int nextInt();
   };
 
   // Set the seed of this random number generator
@@ -25,14 +25,20 @@ namespace javarand
   // Scramble initial seed
   long JavaRandom::initialScramble(long seed)
   {
-    return (this->seed ^ this->multiplier) & this->mask;
+    return (seed ^ this->multiplier) & this->mask;
   }
 
   // Generates the next pseudorandom number
   int JavaRandom::next(int bits)
   {
     this->seed = (this->seed * this->multiplier + this->addend) & this->mask;
-    return (unsigned long)this->seed >> (48 - bits);
+    return (unsigned int)(this->seed >> (48 - bits));
+  }
+
+  // Generate next pseudorandom int
+  int JavaRandom::nextInt()
+  {
+    return this->next(32);
   }
 };
 
