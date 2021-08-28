cddfa7afacdbb73125ecf00ceb8e60d14443f76e
Tiqur <Tiqur7@gmail.com>
2021-08-27 22:38:24 -0700

    Initialize JavaRandom class / constructors

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "random.h"
+#include <iostream>
+
+int main()
+{
+  return 0;
+}
---
+++ random.h
@@ -0,0 +1,17 @@
+namespace javarand
+{
+  class JavaRandom
+  {
+    private:
+      const long multiplier = 0x5DEECE66DL;
+      const long addend = 0xBL;
+      const long mask = (1L << 48) -1;
+
+      long initialScramble(long seed);
+      void setSeed(long seed);
+      int next(int bits);
+
+    public:
+
+  };
+};
