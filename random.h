d6aa99abc42b5671ae19b127ac2ee1e6c4142815
Tiqur <Tiqur7@gmail.com>
2021-08-28 00:19:56 -0700

    Added next public method

--- random.h
+++ random.h
@@ -16,14 +16,23 @@ namespace javarand
 
   };
 
+  // Set the seed of this random number generator
   void JavaRandom::setSeed(long seed)
   {
     this->seed = this->initialScramble(seed);
   }
 
+  // Scramble initial seed
   long JavaRandom::initialScramble(long seed)
   {
     return (this->seed ^ this->multiplier) & this->mask;
+  }
+
+  // Generates the next pseudorandom number
+  int JavaRandom::next(int bits)
+  {
+    this->seed = (this->seed * this->multiplier + this->addend) & this->mask;
+    return (unsigned long)this->seed >> (48 - bits);
   }
 };
 
