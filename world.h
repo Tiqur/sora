8d4c599cf4230ebc463fc0dbce2b8628d32ae766
Tiqur <Tiqur7@gmail.com>
2021-08-28 03:24:41 -0700

    Added isSlimeChunk private method

--- world.h
+++ world.h
@@ -17,5 +17,19 @@ namespace world
       }
   };
 
+  bool World::isSlimeChunk(int x, int z) {
+    std::cout << x << " " << z << std::endl;
+    // Set seed and location
+    rand.setSeed(
+      this->seed +
+      (int) (x * x * 0x4c1906) +
+      (int) (x * 0x5ac0db) +
+      (int) (z * z) * 0x4307a7L +
+      (int) (z * 0x5f24f) ^ 0x3ad8025fL);
+
+    // Determine if slime chunk
+    return !(rand.nextInt(10));
+  }
+
 };
 
