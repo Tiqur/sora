92a6178bf50c161e052ebeab34546ee2122508f7
Tiqur <Tiqur7@gmail.com>
2021-08-28 04:20:02 -0700

    Setup recursive nature of getCluster method

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace world;
 int main()
 {
   World world = World(0, 50);
-  world.printMap(50);
+  //world.printMap(50);
   return 0;
 }
--- world.h
+++ world.h
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
 #include "random.h"
 using namespace javarand;
+
+
+struct coords
+{
+  public:
+    int x;
+    int z;
+};
+
+struct cluster
+{
+  public:
+    coords location;
+    int width;
+    int height;
+};
 
 namespace world
 {
@@ -9,6 +26,7 @@ namespace world
     private:
       int seed;
       JavaRandom rand;
+      cluster getCluster(int x, int z, int depth = 0);
       bool isSlimeChunk(int x, int z);
       void search(int radius);
 
@@ -40,17 +58,43 @@ namespace world
   {
     int half_radius = radius / 2;
     for (int x = -half_radius; x < half_radius; x++)
-    {
       for (int z = -half_radius; z < half_radius; z++)
-      {
-        // If slime chunk, find cluster size
-        if (isSlimeChunk(x, z)) {
-
-        }
-      }
-    }
+        getCluster(x, z, 1);
   }
 
+  // Recursively search for nearby slime chunks within cluster and return dimensions
+  cluster World::getCluster(int x, int z, int depth)
+  {
+    // If not slime chunk, return false;
+    if (!this->isSlimeChunk(x, z)) return cluster{};
+
+    // Holds coordinates of checked chunks ( initialized to 250 since it's virtually impossible for a cluster to be that size  )
+    static std::vector<coords> checked_chunks;
+
+    // If initial cluster check, clear checked_chunks
+    if (depth)
+      checked_chunks.clear();
+
+    // Push self to checked chunks
+    checked_chunks.push_back(coords{x, z});
+
+    std::cout << checked_chunks.size() << std::endl;
+
+    // Check sides
+    //getCluster(x+1, z);
+    //getCluster(x-1, z);
+    //getCluster(x, z+1);
+    //getCluster(x, z-1);
+
+    // Check corners
+    //getCluster(x-1, z+1);
+    //getCluster(x+1, z-1);
+    //getCluster(x+1, z+1);
+    //getCluster(x-1, z-1);
+
+  }
+
+  // Print map to console
   void World::printMap(int radius)
   {
     int half_radius = radius / 2;
