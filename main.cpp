0d0b6b24d9a62f0f375862b41e3b35c612051068
Tiqur <Tiqur7@gmail.com>
2021-08-29 04:06:06 -0700

    Added random seed generationa and main loop

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <random>
+#include <climits>
 #include "world.h"
 using namespace world;
 
+const int chunk_radius = 10000;
+const int spacing = 2; // Default 1
+const int min_size = 14;
+
 int main()
 {
-  int chunk_radius = 10000;
-  int spacing = 3; // Default 1
-  int min_size = 14;
-  World world = World(11, chunk_radius, min_size, spacing);
-  //world.printMap(20);
+  // Generate random number
+  std::random_device rd;
+  std::mt19937_64 eng(rd());
+  std::uniform_int_distribution<long> distr;
+  std::srand(time(NULL));
+
+  // Main loop
+  while (1)
+  {
+    // Generate seed
+    long seed = distr(eng) * (rand() % 2 ? 1 : -1);
+
+    // Search world
+    World world = World(seed, chunk_radius, min_size, spacing);
+  }
   return 0;
 }
