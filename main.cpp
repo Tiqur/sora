94ee21b12beeedd80d0caa8995f4f0e467ad2765
Tiqur <Tiqur7@gmail.com>
2021-08-29 22:03:00 -0700

    Added startup logs

--- main.cpp
+++ main.cpp
@@ -7,11 +7,18 @@ using namespace world;
 const bool logging = true;
 const bool returnOnlyRectangles = true;
 const int chunk_radius = 10000;
-const int spacing = 2; // Default 1
-const int min_size = 8;
+const int spacing = 3; // Default 1
+const int min_size = 9;
 
 int main()
 {
+  std::cout << "Starting..." << std::endl;
+  std::cout << "Logging: " << (logging ? "true" : "false") << std::endl;
+  std::cout << "Return Only Rectangles: " << (returnOnlyRectangles ? "true" : "false") << std::endl; 
+  std::cout << "Search Radius: " << chunk_radius << std::endl;
+  std::cout << "Spacing Optimization: " << spacing << " ( +" << spacing-1 << "00% )" << std::endl;
+  std::cout << "Minimum Cluster Size: " << min_size << std::endl;
+  
   // Generate random number
   std::random_device rd;
   std::mt19937_64 eng(rd());
