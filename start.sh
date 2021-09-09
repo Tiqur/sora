39ee22cce90d7938ca144206774844d3c0b292b5
Tiqur <Tiqur7@gmail.com>
2021-09-08 20:06:25 -0700

    Fix bash file

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-START_TIME="$(date -u +%s)"
 
 # Start search
 parallel ./a.out ::: $(seq $THREADS) &
@@ -7,9 +6,8 @@ parallel ./a.out ::: $(seq $THREADS) &
 # Ping server every second
 while sleep 2; do
   CONNECTED_USERS=$(curl -s -X GET -H "Content-Type: application/json" -d "{\"user\": \"${USER}\"}" http://149.28.75.54/api)
-  TIME_SINCE = "$(($START_TIME - date -u +%s))"
   printf "\033c \033[0;0H---Slime Chunk Cluster Finder---
-Uptime: $TIME_SINCE
+Uptime: $(date +"%T")
 Connected users: $CONNECTED_USERS
 Threads: $THREADS"
 done
