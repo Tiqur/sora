cb9e2edca4efeaa7c1b0dda19b80f1c6dd2ec6c8
Tiqur <Tiqur7@gmail.com>
2021-09-08 18:21:53 -0700

    Show connected users and uptime

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-parallel --lb ./a.out ::: $(seq $THREADS)
+# Start search
+parallel ./a.out ::: $(seq $THREADS) &
+
+# Ping server every second
+while sleep 2; do
+  CONNECTED_USERS=$(curl -s -X GET -H "Content-Type: application/json" -d "{\"user\": \"${USER}\"}" http://149.28.75.54/api)
+  printf "\033c \033[0;0H---Slime Chunk Cluster Finder---
+Uptime: $(date +"%T")
+Connected users: $CONNECTED_USERS
+Threads: $THREADS"
+done
+
