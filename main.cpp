692fb579e3e021d7dba383ff7f2dd6abe6d181b9
Tiqur <Tiqur7@gmail.com>
2021-09-26 01:52:20 -0700

    Fill chunk coordinate values

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 
 const bool logging = true;
 const bool returnOnlyRectangles = true;
-const int chunk_radius = 25;
+const int radius = 25;
 const int spacing = 4; 
 const int min_size = 16;
 const int batch_size = 10;
-const int array_length = std::ceil((float)(chunk_radius * chunk_radius + (chunk_radius % spacing == 0 ? 0 : chunk_radius*2)) / spacing);
+const int array_length = std::ceil((float)(radius * radius + (radius % spacing == 0 ? 0 : radius*2)) / spacing);
 
 int main()
 {
@@ -19,7 +19,7 @@ int main()
   std::cout << "-----------------------------------------------" << std::endl;
   std::cout << "Logging: " << (logging ? "true" : "false") << std::endl;
   std::cout << "Return Only Rectangles: " << (returnOnlyRectangles ? "true" : "false") << std::endl; 
-  std::cout << "Search Radius: " << chunk_radius << std::endl;
+  std::cout << "Search Radius: " << radius << std::endl;
   std::cout << "Spacing Optimization: " << spacing << " ( +" << spacing-1 << "00% )" << std::endl;
   std::cout << "Minimum Cluster Size: " << min_size << std::endl;
   std::cout << "-----------------------------------------------" << std::endl;
@@ -27,22 +27,27 @@ int main()
   // Initialize generator with different value each time
   af::setSeed(time(NULL));
 
+  // Initialize chunk coordinate values
+  int x_coords[array_length];
+  int z_coords[array_length];
+
+  // Generate and fill chunk cordinate values
+  int index = 0;
+  for (int x = -radius; x < radius; x+=spacing)
+    for (int z = -radius; z < radius; z+=spacing)
+    {
+      x_coords[index] = x;
+      z_coords[index] = z;
+      index++;
+    }
+
+  // Coordinate chunk values
+  af::array X = af::array(1, array_length, x_coords);
+  af::array Z = af::array(1, array_length, z_coords);
+
   // Create matrix of random values ( seeds )
   af::array SEEDS = af::randu(batch_size, s64);
   af_print(SEEDS)
-  std::cout << array_length << std::endl;
-
-
-
-  // Main loop
-  //while (true)
-  //{
-  //  // Generate seed
-  //  long long int seed = distr(eng) - 0x8000000000000000;
-
-  //  // Search world
-  //  world::World world = world::World(seed, chunk_radius, min_size, spacing, logging, returnOnlyRectangles, cached_coordinate_values, array_size, false);
-  //}
-
-  return 0;
+  af_print(X)
+  af_print(Z)
 }
